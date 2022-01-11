c9bef1e4267a8d95aae7bf2622a8fcb7bcd9de3e
yakzaetz <yakzaetz@gmail.com>
2022-01-11 17:15:33 -0500

    creating user through api

--- first_routes/app/controllers/users_controller.rb
+++ first_routes/app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
     def index
-        render plain: "I'm in the index actions!"
+        # render plain: "I'm in the index actions!"
+        render json: User.all
     end
 
     def create
@@ -8,11 +9,18 @@ class UsersController < ApplicationController
     end 
 
     def show
-        # user = @users.find(params[:id])
-        # if @users 
-        #     render json: user
-        # else
+        user = @users.find(params[:id])
+        if @users 
+            render json: user
+        else
             render json: params
-        # end
+        end
+    end
+
+    def create
+        user = User.new(params.require(:name).permit(:user_attributes_here))
+        # replace the `user_attributes_here` with the actual attribute keys
+        user.save!
+        render json: user
     end
 end
