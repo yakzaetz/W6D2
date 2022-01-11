98fb75514c30248ac61e8a2ecc4aba7c455ab518
yakzaetz <yakzaetz@gmail.com>
2022-01-11 15:28:50 -0500

    playing with params

--- first_routes/app/controllers/users_controller.rb
+++ first_routes/app/controllers/users_controller.rb
@@ -2,4 +2,17 @@ class UsersController < ApplicationController
     def index
         render plain: "I'm in the index actions!"
     end
+
+    def create
+        render json: params
+    end 
+
+    def show
+        user = @users.find(params[:id])
+        if @users 
+            render json: user
+        else
+            
+
+    end
 end
